e9abf06e865950996ab38e4171d4bdd6bd59043c
Aidan Taylor <aidan.taylor@uea.ac.uk>
2025-06-12 22:17:33 +0100

    Update loadSegmentationArchive.m
    Updated for compatibility with new version of uiget

--- toolbox/+stormpar/+deeplearning/+io/loadSegmentationArchive.m
+++ toolbox/+stormpar/+deeplearning/+io/loadSegmentationArchive.m
@@ -9,13 +9,10 @@ function [dsImage, dsLabel] = loadSegmentationArchive(filename, nameValueArgs)
 	
 	if isempty(filename)
 		% Promp user to choose a folder(s) and/or file(s) (must be in same folder)
-		[file, location] = stormpar.deeplearning.utility.uiget(pwd, 'Title', 'Choose a NEXRAD Image Archive', 'MultiSelect', true);
+		filename = stormpar.deeplearning.utility.uiget("Title", "Choose a NEXRAD Image Archive", "MultiSelect", true);
 		
-		% If location returns an "empty" string array ("" shows as 1x1 array) assume ui was cancelled and error
-		if isempty(location{:}), error('STORMPAR:IO:InvalidID', 'No local folder or file selected'); end
-		
-		% Form full path to chosen files
-		filename = string(fullfile(location, file));
+		% If uiget returns 0, assume ui was cancelled and error
+		if isnumeric(filename), error("STORMPAR:IO:InvalidCode", "No local folder or file selected"); end
 	end
 	
 	% Pull the imageSize into a string to make a dedicated folder for it.
